23a9b7ef3aed3358a16894c85e4cdcaca294be5e
PurpleHP <ermertay@gmail.com>
2024-07-24 00:33:07 +0300

    Change in home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,10 @@
 const Home = () => {
     return (
-        <div className="flex flex-col h-screen justify-center items-center">
-            <h1 className="text-3xl lg:text-5xl">Biyonca</h1>
-            <div className="flex flex-row">
-                <a href="/Login" className="bg-[#161A1D] text-2xl mx-5 hover:bg-gray-700 hover:scale-125 mt-10 text-white font-bold py-2 px-4 rounded">Login</a>
-                <a href="/Signup" className="bg-[#161A1D] text-2xl mx-5 hover:bg-gray-700 hover:scale-125 mt-10 text-white font-bold py-2 px-4 rounded">SignUp</a>
-
+        <div className="flex flex-col h-screen justify-center items-center bg-[#0B090A]">
+            <h1 className="text-6xl lg:text-9xl text-white font-extrabold">Biyonca</h1>
+            <div className="flex flex-row mt-10 lg:mt-20">
+                <a href="/Login" className="bg-[#161A1D] text-center text-3xl lg:text-5xl mx-5 hover:bg-gray-700 hover:scale-125  text-white font-bold p-4 lg:py-6 lg:px-6 rounded">Login</a>
+                <a href="/Signup" className="bg-[#161A1D] text-center  text-3xl lg:text-5xl mx-5 hover:bg-gray-700 hover:scale-125  text-white font-bold p-4 lg:py-6 lg:px-6 rounded">Sign Up</a>
             </div>
         </div>
     )
