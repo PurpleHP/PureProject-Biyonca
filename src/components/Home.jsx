07d80d1a19c133b4e419dbc70ce140f23ef51e21
PurpleHP <ermertay@gmail.com>
2024-07-24 00:24:29 +0300

    Initial

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,10 @@
 const Home = () => {
     return (
         <div className="flex flex-col h-screen justify-center items-center">
-            <h1>Welcome to the Home Page</h1>
-            <a href="/Login" className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">Login</a>
+            <h1 className="text-3xl lg:text-5xl">Biyonca</h1>
+            <div className="flex flex-row">
+      
+            </div>
         </div>
     )
 }
