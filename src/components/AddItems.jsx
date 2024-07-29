ad38f602e7b0aa77fdf644a32a0479a6f07342cf
PurpleHP <ermertay@gmail.com>
2024-07-29 17:11:35 +0300

    add test

--- src/components/AddItems.jsx
+++ src/components/AddItems.jsx
@@ -1,19 +1,20 @@
 const AddItems = () => {
 
+    const [items, setItems] = useState([]);
     //Adds a container with text and id with a border
     const AddEmptyContainer = () => {
+        setItems([...items, {id: items.length, text: 'Empty Container'}]);
         return (
-            <div className="border border-black w-[50vw]">
+            <div className="border border-black w-[50vw] text-center m-5 p-5">
                 <p>Empty Container</p>
             </div>
         );
     }
 
     return (
-        <div>
+        <div className="justify-center flex h-screen">
             <h1>Add Items</h1>
-            <button >Add Item</button>
-
+            <button onClick={AddEmptyContainer}>Add Empty Container</button>
         </div>
     );
 }
