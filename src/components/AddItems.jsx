e2e9401b5a40160e975195963f9e94285379a3ea
PurpleHP <ermertay@gmail.com>
2024-07-29 17:22:14 +0300

    add items section

--- src/components/AddItems.jsx
+++ src/components/AddItems.jsx
@@ -1,20 +1,33 @@
+import React, { useState } from 'react';
+
 const AddItems = () => {
 
     const [items, setItems] = useState([]);
     //Adds a container with text and id with a border
     const AddEmptyContainer = () => {
-        setItems([...items, {id: items.length, text: 'Empty Container'}]);
-        return (
-            <div className="border border-black w-[50vw] text-center m-5 p-5">
-                <p>Empty Container</p>
-            </div>
-        );
+        setItems([...items, {id: items.length, text: 'Empty Test'}]);
+    }
+
+    const ResetItems = () => {
+        setItems([]);
     }
 
     return (
-        <div className="justify-center flex h-screen">
-            <h1>Add Items</h1>
-            <button onClick={AddEmptyContainer}>Add Empty Container</button>
+        <div className="flex justify-center items-center flex-col h-screen bg-gray-400 text-white">
+            <div className='flex flex-row'>
+                <button className='flex border-2 m-2 p-2' onClick={AddEmptyContainer}>Add New Test</button>
+                <button className='flex border-2 m-2 p-2' onClick={ResetItems}>Reset Tests</button>
+            </div>
+          
+            <div className="flex flex-col">
+                {items.map((item) => (
+                <div key={item.id} className="border justify-center border-black w-[50vw] text-center m-5 p-5">
+                    <p>{item.text}</p>
+                </div>
+                ))}
+            </div>
         </div>
     );
 }
+
+export default AddItems;
