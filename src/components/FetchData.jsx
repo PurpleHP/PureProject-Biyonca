6e089c7d6c65550ae46b2c8b6c3c33aac005f98d
PurpleHP <ermertay@gmail.com>
2024-09-01 23:56:55 +0300

    biyonca text added + small changes

--- src/components/FetchData.jsx
+++ src/components/FetchData.jsx
@@ -222,196 +222,200 @@ const FetchData = () => {
   return (
 
     <div className="bg-[#222831] min-h-screen flex flex-col w-full justify-start items-center overflow-y-auto">
-    <div className="bg-[#222831] flex flex-row fixed items-center justify-center top-0 left-0 w-full z-50 p-4">
-      <button className="m-2 font-bold text-[#EEEEEE] bg-[#31363F] shadow-2xl shadow-black rounded-lg p-4 hover:bg-[#292d35] hover:scale-105 transition-transform duration-300" onClick={filterOptions}>
-        Filter
-      </button>
-      <button className="m-2 font-bold text-[#EEEEEE] bg-[#3D8B3D] shadow-2xl shadow-black rounded-lg p-4 hover:bg-[#2E6B2E] hover:scale-105 transition-transform duration-300" onClick={fetchDataButton}>
-        View All Results
-      </button>
-      <button className="m-2 font-bold text-[#EEEEEE] bg-[#D9534F] shadow-2xl shadow-black rounded-lg p-4 hover:bg-[#C14440] hover:scale-105 transition-transform duration-300" onClick={clearData}>
-        Log Out
-      </button>
-    </div>
-
-    <div className="mt-24 grid grid-cols-1 md:grid-cols-4 gap-5">
-    {allTestInfo && allTestInfo.map((info, index) => (
-        <div key={index} className="p-5 m-5 shadow-2xl shadow-black rounded bg-[#31363F]">
-          <p className='mb-2'>
-            <span className="text-[#EEEEEE] text-2xl">Name: </span>
-            <span className="text-[#76ABAE] text-2xl">{info.name}</span>
-          </p>
-       
-          <p className='mb-1'>
-            <span className="text-[#EEEEEE] text-xl">Test Number: </span>
-            <span className="text-[#76ABAE] text-xl">{info.testNum}</span>
-          </p>
-          <p className="text-[#EEEEEE]"> {info.result}</p>
-          <p className='mb-2'>
-            <span className="text-[#EEEEEE] text-xl">Test Result: </span>
-            <span className="text-[#76ABAE] text-xl">{info.result}</span>
-          </p>
-          <p>
-            <span className="text-[#EEEEEE]">Date: </span>
-            <span className="text-[#76ABAE]">{info.created}</span>
-          </p>
-        </div>
-      ))}
+        <div className="bg-[#222831] flex flex-col fixed items-center justify-center top-0 left-0 w-full z-50 p-4">
+          <h className="md:text-7xl text-6xl text-[#EEEEEE] font-bold">Biyonca</h>
+          <div className='pt-4'>
+            <button className="m-2 font-bold text-[#EEEEEE] bg-[#31363F] shadow-2xl shadow-black rounded-lg p-4 hover:bg-[#292d35] hover:scale-105 transition-transform duration-300" onClick={filterOptions}>
+              Filter
+            </button>
+            <button className="m-2 font-bold text-[#EEEEEE] bg-[#3D8B3D] shadow-2xl shadow-black rounded-lg p-4 hover:bg-[#2E6B2E] hover:scale-105 transition-transform duration-300" onClick={fetchDataButton}>
+              View All Results
+            </button>
+            <button className="m-2 font-bold text-[#EEEEEE] bg-[#D9534F] shadow-2xl shadow-black rounded-lg p-4 hover:bg-[#C14440] hover:scale-105 transition-transform duration-300" onClick={clearData}>
+              Log Out
+            </button>
+          </div>
       </div>
+        
 
-      {/* Logout */}
+      <div className="mt-44 grid grid-cols-1 md:grid-cols-4 gap-5">
+      {allTestInfo && allTestInfo.map((info, index) => (
+          <div key={index} className="p-5 m-5 shadow-2xl shadow-black rounded bg-[#31363F]">
+            <p className='mb-2'>
+              <span className="text-[#EEEEEE] text-2xl">Name: </span>
+              <span className="text-[#76ABAE] text-2xl">{info.name}</span>
+            </p>
+          
+            <p className='mb-1'>
+              <span className="text-[#EEEEEE] text-xl">Test Number: </span>
+              <span className="text-[#76ABAE] text-xl">{info.testNum}</span>
+            </p>
+            <p className="text-[#EEEEEE]"> {info.result}</p>
+            <p className='mb-2'>
+              <span className="text-[#EEEEEE] text-xl">Test Result: </span>
+              <span className="text-[#76ABAE] text-xl">{info.result}</span>
+            </p>
+            <p>
+              <span className="text-[#EEEEEE]">Date: </span>
+              <span className="text-[#76ABAE]">{info.created}</span>
+            </p>
+          </div>
+        ))}
+        </div>
 
-      {showConfirmation && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-[#31363F] p-6 rounded-lg shadow-md">
-            <p className="text-[#EEEEEE] mb-4">Are you sure you want to log out?</p>
-            <div className="flex justify-end">
-            <button
-                onClick={() => setShowConfirmation(false)}
+        {/* Logout */}
 
-                className="bg-[#D9534F] text-[#EEEEEE] p-2 mr-2 rounded hover:bg-[#b14440] hover:scale-105 transition-transform duration-300"
-              >
+        {showConfirmation && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+            <div className="bg-[#31363F] p-6 rounded-lg shadow-md">
+              <p className="text-[#EEEEEE] mb-4">Are you sure you want to log out?</p>
+              <div className="flex justify-end">
+              <button
+                  onClick={() => setShowConfirmation(false)}
+
+                  className="bg-[#D9534F] text-[#EEEEEE] p-2 mr-2 rounded hover:bg-[#b14440] hover:scale-105 transition-transform duration-300"
+                >
+                  Cancel
+                </button>
+                <button
+                  onClick={handleLogout}
+                  className="bg-[#3D8B3D] text-[#EEEEEE] p-2 rounded ml-2 hover:bg-[#2E6B2E] hover:scale-105 transition-transform duration-300"
+                >
+                  Confirm
+                </button>
+            
+              </div>
+            </div>
+          </div>
+        )}
+
+        {/* Filter */}
+
+        {showFilter && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+            <div className="bg-[#31363F] p-6 rounded-lg shadow-md">
+              <div className="flex relative flex-col justify-center items-center text-center md:w-[50vw] w-[80vw] h-[60vh]">
+              <button className="absolute top-0 right-0 mt-2 ml-2 mb-2 font-bold text-[#EEEEEE] bg-[#D9534F] shadow-2xl shadow-black rounded-lg p-4 hover:bg-[#C14440] hover:scale-105 transition-transform duration-300" onClick={() => setShowFilter(false)}>
                 Cancel
               </button>
-              <button
-                onClick={handleLogout}
-                className="bg-[#3D8B3D] text-[#EEEEEE] p-2 rounded ml-2 hover:bg-[#2E6B2E] hover:scale-105 transition-transform duration-300"
-              >
-                Confirm
-              </button>
-          
-            </div>
-          </div>
-        </div>
-      )}
-
-      {/* Filter */}
-
-      {showFilter && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-[#31363F] p-6 rounded-lg shadow-md">
-            <div className="flex relative flex-col justify-center items-center text-center md:w-[50vw] w-[80vw] h-[60vh]">
-            <button className="absolute top-0 right-0 mt-2 ml-2 mb-2 font-bold text-[#EEEEEE] bg-[#D9534F] shadow-2xl shadow-black rounded-lg p-4 hover:bg-[#C14440] hover:scale-105 transition-transform duration-300" onClick={() => setShowFilter(false)}>
-              Cancel
-            </button>
-            <p className="text-[#EEEEEE] absolute font-bold mt-2 p-4 top-0 md:text-3xl text-2xl mb-12">Filter Options</p>
-              {/* Test Number */}
-              <div className="mb-4 mt-16 w-full">
-                <p className="text-[#EEEEEE] mb-4">Test Number</p>
-                <Range
-                  step={1}
-                  min={minVal}
-                  max={maxVal}
-                  values={values}
-                  onChange={(values) => setValues(values)}
-                  renderTrack={({ props, children }) => (
-                    <div
-                      {...props}
-                      style={{
-                        ...props.style,
-                        height: '8px',
-                        width: '100%',
-                        backgroundColor: '#ccc'
-                      }}
-                    >
-                      {children}
+              <p className="text-[#EEEEEE] absolute font-bold mt-2 p-4 top-0 md:text-3xl text-2xl mb-12">Filter Options</p>
+                {/* Test Number */}
+                <div className="mb-4 mt-16 w-full">
+                  <p className="text-[#EEEEEE] mb-4">Test Number</p>
+                  <Range
+                    step={1}
+                    min={minVal}
+                    max={maxVal}
+                    values={values}
+                    onChange={(values) => setValues(values)}
+                    renderTrack={({ props, children }) => (
+                      <div
+                        {...props}
+                        style={{
+                          ...props.style,
+                          height: '8px',
+                          width: '100%',
+                          backgroundColor: '#ccc'
+                        }}
+                      >
+                        {children}
+                      </div>
+                    )}
+                    renderThumb={({ props, index }) => (
+                      <div
+                        {...props}
+                        style={{
+                          ...props.style,
+                          height: '24px',
+                          width: '24px',
+                          backgroundColor: index === 0 ? '#3D8B3D' : '#D9534F',
+                          borderRadius: '50%'
+                        }}
+                      />
+                    )}
+                  />
+                  <div className="flex justify-between mt-2">
+                    <div className="flex flex-col items-center">
+                      <label className="text-[#EEEEEE]">Min:</label>
+                      <input
+                        type="number"
+                        value={values[0]}
+                        min={minVal}
+                        max={maxVal}
+                        onChange={(e) => handleInputChange(0, Number(e.target.value))}
+                        className="p-2 rounded bg-[#EEEEEE] text-[#31363F] w-[2wv]"
+                      />
                     </div>
-                  )}
-                  renderThumb={({ props, index }) => (
-                    <div
-                      {...props}
-                      style={{
-                        ...props.style,
-                        height: '24px',
-                        width: '24px',
-                        backgroundColor: index === 0 ? '#3D8B3D' : '#D9534F',
-                        borderRadius: '50%'
-                      }}
-                    />
-                  )}
-                />
-                <div className="flex justify-between mt-2">
-                  <div className="flex flex-col items-center">
-                    <label className="text-[#EEEEEE]">Min:</label>
-                    <input
-                      type="number"
-                      value={values[0]}
-                      min={minVal}
-                      max={maxVal}
-                      onChange={(e) => handleInputChange(0, Number(e.target.value))}
-                      className="p-2 rounded bg-[#EEEEEE] text-[#31363F] w-[2wv]"
-                    />
-                  </div>
-                  <div className="flex flex-col items-center">
-                    <label className="text-[#EEEEEE]">Max:</label>
-                    <input
-                      type="number"
-                      value={values[1]}
-                      min={minVal}
-                      max={maxVal}
-                      onChange={(e) => handleInputChange(1, Number(e.target.value))}
-                      className="p-2 rounded bg-[#EEEEEE] text-[#31363F] w-[3wv]"
-                    />
+                    <div className="flex flex-col items-center">
+                      <label className="text-[#EEEEEE]">Max:</label>
+                      <input
+                        type="number"
+                        value={values[1]}
+                        min={minVal}
+                        max={maxVal}
+                        onChange={(e) => handleInputChange(1, Number(e.target.value))}
+                        className="p-2 rounded bg-[#EEEEEE] text-[#31363F] w-[3wv]"
+                      />
+                    </div>
                   </div>
                 </div>
-              </div>
-              {/* Test Number */}
+                {/* Test Number */}
 
-              {/* Test Name */}
-              <div className="mb-4 w-full flex flex-row">
-                <div className="mb-4 w-full flex flex-col">
-                  <p className="text-[#EEEEEE] mb-4">Test Name</p>
-                  <input 
-                  id='testName'
-                  className="p-2 rounded bg-[#EEEEEE] text-[#31363F] mr-2 w-full" 
-                  placeholder='(Default: All)'
-                />
-                </div>
-                <div className="mb-4 w-full flex flex-col">
-                  <p className="text-[#EEEEEE] mb-4">Test Result</p>
-                 <input 
-                 id='testResult'
-                  className="p-2 rounded bg-[#EEEEEE] text-[#31363F] ml-2 w-full" 
-                  placeholder='(Default: All)'
-                />
-                </div>
-
-              </div>
-              {/* Test Name */}
-
-              {/* Date Picker */}
-              <div className="mb-4 w-full flex flex-col">
-                <p className="text-[#EEEEEE] mb-4">Date Range</p>
+                {/* Test Name */}
                 <div className="mb-4 w-full flex flex-row">
-                  
+                  <div className="mb-4 w-full flex flex-col">
+                    <p className="text-[#EEEEEE] mb-4">Test Name</p>
+                    <input 
+                    id='testName'
+                    className="p-2 rounded bg-[#EEEEEE] text-[#31363F] mr-2 w-full" 
+                    placeholder='(Default: All)'
+                  />
+                  </div>
+                  <div className="mb-4 w-full flex flex-col">
+                    <p className="text-[#EEEEEE] mb-4">Test Result</p>
+                    <input 
+                    id='testResult'
+                    className="p-2 rounded bg-[#EEEEEE] text-[#31363F] ml-2 w-full" 
+                    placeholder='(Default: All)'
+                  />
+                  </div>
+
+                </div>
+                {/* Test Name */}
+
+                {/* Date Picker */}
+                <div className="mb-4 w-full flex flex-col">
+                  <p className="text-[#EEEEEE] mb-4">Date Range</p>
+                  <div className="mb-4 w-full flex flex-row">
+                    
+                      <input 
+                        type="date"
+                        id="testStartDate"
+                        className="p-2 rounded bg-[#EEEEEE] text-[#31363F] w-full"
+                        value={startDate}
+                        onChange={handleStartDateChange}
+                        max={endDate}
+                      />
+                    <p className='text-[#EEEEEE] font-extrabold p-2'>-</p>
                     <input 
                       type="date"
-                      id="testStartDate"
+                      id="testEndDate"
                       className="p-2 rounded bg-[#EEEEEE] text-[#31363F] w-full"
-                      value={startDate}
-                      onChange={handleStartDateChange}
-                      max={endDate}
+                      value={endDate}
+                      onChange={handleEndDateChange}
+                      min={startDate}
                     />
-                  <p className='text-[#EEEEEE] font-extrabold p-2'>-</p>
-                  <input 
-                    type="date"
-                    id="testEndDate"
-                    className="p-2 rounded bg-[#EEEEEE] text-[#31363F] w-full"
-                    value={endDate}
-                    onChange={handleEndDateChange}
-                    min={startDate}
-                  />
+                  </div>
                 </div>
-              </div>
-              {/* Date Picker */}
+                {/* Date Picker */}
 
-              <button className="m-2 font-bold text-[#EEEEEE] bg-[#3D8B3D] shadow-2xl shadow-black rounded-lg p-4 hover:bg-[#2E6B2E] hover:scale-105 transition-transform duration-300" onClick={handleApplyFilter}>
-                  View Results
-              </button>
+                <button className="m-2 font-bold text-[#EEEEEE] bg-[#3D8B3D] shadow-2xl shadow-black rounded-lg p-4 hover:bg-[#2E6B2E] hover:scale-105 transition-transform duration-300" onClick={handleApplyFilter}>
+                    View Results
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
     </div>
   );
 }
